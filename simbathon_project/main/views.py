c063aa8786e9cbc29f86379cd3a85bb3ac33f395
Lee Sangdon <88819145+leeideal@users.noreply.github.com>
2022-06-29 18:02:22 +0900

    intropage
    인트로 페이지 제작

--- simbathon_project/main/views.py
+++ simbathon_project/main/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
 def showmain(request):
     return render(request, 'main/mainpage.html')
+
+
+def intro(request):
+    return render(request, 'main/intro.html')
