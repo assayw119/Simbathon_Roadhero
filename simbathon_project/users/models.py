be85bf01f366a42535edc866775c2f21977acdc3
assayw119 <assayw119@naver.com>
2022-07-03 21:25:34 +0900

    mypage를 user에서 accounts로 앱 이동

--- simbathon_project/users/models.py
+++ simbathon_project/users/models.py
@@ -1,10 +1,3 @@
-# from django.db import models
-# from django.contrib.auth.models import User
-# from django.conf import settings
+from django.db import models
 
-# # Create your models here.
-# class Profile(models.Model):
-#     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, primary_key=True)
-#     name = models.CharField(max_length=20, default='', null=True)
-#     university = models.CharField(max_length=20, default='', null=True)
-#     major = models.CharField(max_length=20, default='', null=True)
+# Create your models here.
--- simbathon_project/users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-app_name = 'users'
-urlpatterns = [
-    # path('mypage/', mypage, name='mypage'),
-    # path('newInfo/', newInfo, name='newinfo'),
-    # path('createinfo/', createInfo, name='createinfo'),
-]
