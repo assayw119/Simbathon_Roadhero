0cb8372eecded2eab024214dd67f7f17b98e8d65
assayw119 <assayw119@naver.com>
2022-07-05 16:31:48 +0900

    community create 에러 해결

--- simbathon_project/main/forms.py
+++ simbathon_project/main/forms.py
@@ -1,7 +1,12 @@
 from django import forms
-from .models import Post
+from .models import Post, Community
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title','writer','pub_date','body','category','image']
+
+class CommunityForm(forms.ModelForm):
+    class Meta:
+        model = Community
+        fields = ['title','writer','pub_date','body', 'category']
--- simbathon_project/main/urls.py
+++ simbathon_project/main/urls.py
@@ -9,8 +9,9 @@ urlpatterns = [
     path('new/', new, name='new'),
     path('create/', create, name='create'),
     path('<str:id>', detail, name='detail'),
+    
     path('community/', community, name='community'),
-    path('<str:id>/', community_detail, name='community_detail'),
+    path('community/<str:id>/', community_detail, name='community_detail'),
     path('community_new/', community_new, name='community_new'),
     path('community_create/', community_create, name='community_create'),
 ]
