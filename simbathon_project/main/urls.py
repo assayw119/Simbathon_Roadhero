e05a06ad8c26ff1dcac69fa597e210744ae7e7bc
Lee Sangdon <88819145+leeideal@users.noreply.github.com>
2022-07-08 03:35:38 +0900

    자질구질한것들
    자질구질한것들

--- simbathon_project/main/urls.py
+++ simbathon_project/main/urls.py
@@ -11,11 +11,13 @@ urlpatterns = [
     path('<str:id>', detail, name='detail'),
     path('/', SearchFormView.as_view(), name='search'),
     path('<str:id>/likes/', likes, name='likes'),
-    
+
     path('community/', community, name='community'),
     path('community/<str:id>/', community_detail, name='community_detail'),
     path('community_new/', community_new, name='community_new'),
     path('community_create/', community_create, name='community_create'),
+    path('community_update/', community_update, name='community_update'),
+    path('<str:id>/community_delete', community_delete, name='community_delete'),
 
     path('<str:id>/comment_create', comment_create, name="comment_create"),
     path('<str:id>/comment_delete', comment_delete, name="comment_delete"),
