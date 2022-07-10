56759fc89d91fcac5285d464086d2132865cc083
Lee Sangdon <88819145+leeideal@users.noreply.github.com>
2022-07-10 14:28:56 +0900

    fix mainpage bug
    fix mainpage bug

--- simbathon_project/main/models.py
+++ simbathon_project/main/models.py
@@ -7,27 +7,31 @@ from django.contrib.auth.models import User
 from django.conf import settings
 
 post_choice = (
-    ('취업','취업'),
-    ('창업','창업'),
-    ('진학','진학'),
-    ('기타','기타'),
+    ('취업', '취업'),
+    ('창업', '창업'),
+    ('진학', '진학'),
+    ('기타', '기타'),
 )
 
 write_choice = (
-    ('질문','질문'),
-    ('정보','정보'),
-    ('생활','생활'),
-    ('기타','기타'),
+    ('질문', '질문'),
+    ('정보', '정보'),
+    ('생활', '생활'),
+    ('기타', '기타'),
 )
+
+
 class Post(models.Model):
     title = models.CharField(max_length=100)
     writer = models.ForeignKey(User, on_delete=models.CASCADE)
     pub_date = models.DateTimeField()
     body = models.TextField()
-    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_posts')
+    like_users = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name='like_posts')
     view_users = models.IntegerField(default=0)
-    category = models.CharField(max_length=10, choices=post_choice, default='취업')
-    image = models.ImageField(upload_to='media/post/', blank=True, null=True)
+    category = models.CharField(
+        max_length=10, choices=post_choice, default='취업')
+    image = models.ImageField(upload_to='post/', blank=True, null=True)
 
     def __str__(self):
         return self.title
@@ -39,12 +43,15 @@ class Post(models.Model):
         self.view_users += 1
         self.save()
 
+
 class Comment(models.Model):
     content = models.TextField()
     writer = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post ,on_delete=models.CASCADE, related_name ='comments')
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='comments')
     created_at = models.DateTimeField(auto_now=True)
     update_at = models.DateTimeField(auto_now=True)
+
 
 class Community(models.Model):
     id = models.AutoField(primary_key=True)
@@ -52,27 +59,30 @@ class Community(models.Model):
     writer = models.ForeignKey(User, on_delete=models.CASCADE)
     pub_date = models.DateTimeField()
     body = models.TextField()
-    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='community_like_posts')
+    like_users = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name='community_like_posts')
     view_users = models.IntegerField(default=0)
-    category = models.CharField(max_length=10, choices=write_choice, default='질문')
+    category = models.CharField(
+        max_length=10, choices=write_choice, default='질문')
     image = models.ImageField(upload_to='community/', blank=True, null=True)
 
     def summary(self):
         if len(self.body) >= 50:
-            return self.body + '...'
+            return self.body[:50] + '...'
         else:
             return self.body
+
+
 class CommunityComment(models.Model):
     content = models.TextField()
     writer = models.ForeignKey(User, on_delete=models.CASCADE)
-    community = models.ForeignKey(Community ,on_delete=models.CASCADE, related_name ='communitycomments')
+    community = models.ForeignKey(
+        Community, on_delete=models.CASCADE, related_name='communitycomments')
     created_at = models.DateTimeField(auto_now=True)
     update_at = models.DateTimeField(auto_now=True)
-
 
     def summary(self):
         if len(self.body) >= 100:
             return self.body[:100]
         else:
             return self.body[:100]
-    
